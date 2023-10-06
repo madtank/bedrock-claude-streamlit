3f099b799637f38df29795de40c06c98df7c4374
Jacob Taunton <jandrewt82@gmail.com>
2023-10-05 23:12:03 -0700

    converstation memory fix

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 Claude v2 Chatbot Streamlit Application
 =======================================
 
-This application provides an interface to interact with the Claude v2 chatbot using Streamlit.
+This application provides an interface to interact with the AWS Bedrock Claude chatbot using Streamlit.
 
 Instructions:
 -------------
 1. Ensure you have Python installed on your system.
 2. Install the required Python packages using the following command:
    ```
-   pip install streamlit langchain
+   pip install streamlit langchain boto3
    ```
 3. Navigate to the directory containing the `claude_app.py` file.
 4. Run the Streamlit application with the following command:
    ```
-   streamlit run claude_app.py
+   streamlit run bedrock_claude.py
    ```
 
 This should launch a web browser displaying the Streamlit interface for the Claude v2 chatbot. You can then interact with the chatbot directly from the web interface.
--- bedrock_claude.py
+++ bedrock_claude.py
@@ -1,7 +1,9 @@
+
 import boto3
 import json
 import os
 import sys
+import streamlit as st
 
 from langchain.chains import ConversationChain
 from langchain.llms.bedrock import Bedrock
@@ -9,6 +11,8 @@ from langchain import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
 
+
+st.title("AWS Bedrock with Claude V2")
 region = "us-west-2"
 
 # Instantiate the model
@@ -17,15 +21,17 @@ cl_llm = Bedrock(
     region_name=region,
     model_kwargs={"max_tokens_to_sample": 500}
     )
-memory = ConversationBufferMemory()
+
+# Claude needs to know the prefix of the AI's name in the conversation Assistant
+memory = ConversationBufferMemory(ai_prefix="Assistant")
 conversation = ConversationChain(
-    llm=cl_llm, verbose=False, memory=ConversationBufferMemory()
+    llm=cl_llm, verbose=False, memory=ConversationBufferMemory(ai_prefix="Assistant")
     )
 
 # Update the prompt template
 claude_prompt = PromptTemplate.from_template("""The following is a friendly conversation between a human and an AI.
 Keep the answers short and concise. If the AI does not know
-the answer to a question, it truthfully says it does not know.                                                                                
+the answer to a question, it truthfully says it does not know.                                                                               
 
 Current conversation:
 {history}
@@ -40,5 +46,29 @@ conversation.prompt = claude_prompt
 
 def get_answer_from_query(query):
     result = conversation.predict(input=query)
-    print('Output from get_answer_from_query:', result)
     return result
+
+# If the messages and conversation memory are not in session state, initialize them
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "conversation_memory" not in st.session_state:
+    st.session_state.conversation_memory = memory
+
+# Update the conversation's memory with the session state's memory
+conversation.memory = st.session_state.conversation_memory
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = get_answer_from_query(prompt)
+        st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    # Update the session state memory after the conversation
+    st.session_state.conversation_memory = conversation.memory

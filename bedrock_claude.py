2e2c0df19c18096395c86da362c8260a6c207f94
Jacob Taunton <jandrewt82@gmail.com>
2023-10-12 22:45:28 -0700

    adding persona

--- bedrock_claude.py
+++ bedrock_claude.py
@@ -15,6 +15,37 @@ from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_
 st.title("AWS Bedrock with Claude V2")
 region = "us-west-2"
 
+# streamlit sidebar
+# Initialize session_state for 'persona' if not already initialized
+if 'persona' not in st.session_state:
+    st.session_state.persona = "Friendly AI"
+
+# Sidebar for persona selection and chat reset
+with st.sidebar:
+    # Choose a Persona
+    persona_option = st.selectbox('Choose a Persona:', ['Friendly AI', 'Dev', 'Guru', 'Comedian'], key='persona_selectbox')
+    if persona_option:
+        st.session_state.persona = persona_option
+
+    # Show selected persona and option to reset
+    if st.session_state.persona:
+        st.write(f"Selected Persona: {st.session_state.persona}")
+        if st.button("Clear Chat"):
+            st.session_state.persona = "Friendly AI"
+            st.session_state.messages = []  # Clear the chat messages
+            st.session_state.conversation_memory = ConversationBufferMemory(ai_prefix="Assistant")  # Reset the conversation memory
+
+# Mapping of persona to prompt
+persona_to_prompt = {
+    'Friendly AI': 'The following is a friendly conversation between a human and an AI.',
+    'Dev': 'I want you to act as a software developer.',
+    'Guru': 'I want you to act as a yogi.',
+    'Comedian': 'I want you to act as a comedian.'
+}
+
+# Display prompt based on the persona
+prompt = persona_to_prompt[st.session_state.persona]
+
 # Instantiate the model
 cl_llm = Bedrock(
     model_id="anthropic.claude-v2", 
@@ -29,20 +60,21 @@ conversation = ConversationChain(
     )
 
 # Update the prompt template
-claude_prompt = PromptTemplate.from_template("""The following is a friendly conversation between a human and an AI.
-Keep the answers short and concise. If the AI does not know
-the answer to a question, it truthfully says it does not know.                                                                               
-
+# Update the prompt template
+claude_prompt = PromptTemplate.from_template(f"""Human: {prompt}
+                                             
+Assistant: Yes, I will be a helpful AI and play the part.
+                                                       
 Current conversation:
-{history}
+{{history}}
 
-
-Human: {input}
-
+Human: {{input}}
 
 Assistant:
 """)
+
 conversation.prompt = claude_prompt
+print(conversation.prompt)
 
 def get_answer_from_query(query):
     result = conversation.predict(input=query)
